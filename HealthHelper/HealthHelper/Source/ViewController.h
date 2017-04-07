58d0161de2404a2a08ff4704259e91b753523b51
Maksym Savisko <maks.savisko.triare@gmail.com>
2017-04-07 16:43:36 +0300

    [Core] Init folders. Init pod file.

---
+++ HealthHelper/HealthHelper/Source/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HealthHelper
+//
+//  Created by Maksym Savisko on 4/7/17.
+//  Copyright © 2017 Maksym Savisko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

3edaeec5cc43efcf850fb6f3fa0528572fe96ff1
Maksym Savisko <maks.savisko.triare@gmail.com>
2017-04-10 11:33:46 +0300

    [Purchase] Add categories.

---
+++ HealthHelper/HealthHelper/Source/StoreManager/Categories/MSDetailedDescriptionProtocol.h
@@ -0,0 +1,15 @@
+//
+//  MSDetailedDescriptionProtocol.h
+//  HealthHelper
+//
+//  Created by Maksym Savisko on 4/10/17.
+//  Copyright © 2017 Maksym Savisko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol MSDetailedDescriptionProtocol <NSObject>
+
+@property (nonatomic, readonly) NSString *ms_detailedDescription;
+
+@end
---
+++ HealthHelper/HealthHelper/Source/StoreManager/Categories/SKPayment+MS.h
@@ -0,0 +1,14 @@
+//
+//  SKPayment+MS.h
+//  HealthHelper
+//
+//  Created by Maksym Savisko on 4/10/17.
+//  Copyright © 2017 Maksym Savisko. All rights reserved.
+//
+
+#import <StoreKit/StoreKit.h>
+#import "MSDetailedDescriptionProtocol.h"
+
+@interface SKPayment (MS) <MSDetailedDescriptionProtocol>
+
+@end
